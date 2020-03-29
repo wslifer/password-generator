3f09ba9a02ee8245dc5df921579fbef4250a5c80
Wesley Slifer <wesleyslifer@ymail.com>
2020-03-29 15:58:21 -0500

    fixed for loop and changed my vars to arrays.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var num = "0123456789";
-var spc = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var num = ['1','2','3','4','5','6','7','8','9','0'];
+var spc = ['!','@','#','$','%','^','&','*','(',')','_','-','[','{',']','}',';',':','/','.','>','<',',','`','~','=','+','*'];
 
 // Write password to the #password input
 function writePassword() {
@@ -27,26 +27,24 @@ function generatePassword() {
   }
   else {
     if (confirm('Would you like to use special characters?')) {
-      chars.push(spc);
+      Array.prototype.push.apply(chars, spc);
     };
     if (confirm('Would you like to use numbers?')) {
-      chars.push(num);
+      Array.prototype.push.apply(chars, num);
     };
     if (confirm('Would you like to use lower case letters?')) {
-      chars.push(lower);
+      Array.prototype.push.apply(chars, lower);
     };
     if (confirm('Would you like to use upper case letters?')) {
-      chars.push(upper);
+      Array.prototype.push.apply(chars, upper);
     };
     if (chars.length === 0) {
       alert('Password must contain characters');
-    }
-    else {
-      for (var i=0; i<passLength; i++){
-        var randPass = Math.floor(Math.random()*chars.length);
-        password = chars[randPass];
-      }
+    };
+    for (var i = 0; i < passLength; i++) {
+      var randPass = Math.floor(Math.random() * chars.length);
+      password += chars[randPass];
+
     };
   };
-  return randPass;
-}
+  return password;}
